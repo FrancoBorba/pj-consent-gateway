4e49ee8bed35bb9b88ace4c69526afdf22f271be
Franco Borba <franco.borba14@gmail.com>
2025-09-29 14:09:59 -0300

    Adds backend and frontend frameworks

---
+++ backend/src/main/java/com/Gateway/PJ/GatewayPjApplication.java
@@ -0,0 +1,13 @@
+package com.Gateway.PJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayPjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayPjApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/Gateway/PJ/GatewayPjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gateway.PJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayPjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
